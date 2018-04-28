b62bfddbbcdd41360d6d328edf29d42ca067cf30
Matthew Copple <mcopple@gmail.com>
2018-04-28 01:15:49 -0500

    final commit

--- analysis_service.py
+++
@@ -1,7 +0,0 @@
-# Import and Initialize Sentiment Analyzer
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-analyzer = SentimentIntensityAnalyzer()
-
-def analyze_tweets(tweet):
-    # Run Vader Analysis on each tweet
-    return analyzer.polarity_scores(tweet["text"])
